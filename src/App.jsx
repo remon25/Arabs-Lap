a6eeaa3e1ff3eed023ddaa7e2cdd2ce1cecef86b
remon25 <70450564+remon25@users.noreply.github.com>
2024-06-24 02:55:51 +0300

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-// import Dashboard from "./pages/Dashboard";
+import Dashboard from "./pages/Dashboard";
 import OperationReport from "./pages/OperationReport";
 import LabReport from "./pages/LabReport";
 import Users from "./pages/Users";
@@ -41,7 +41,7 @@ function App() {
               }
             >
               <Route index element={<Navigate replace to="dashboard" />} />
-              {/* <Route path="/dashboard" element={<Dashboard />} /> */}
+              <Route path="/dashboard" element={<Dashboard />} />
               <Route path="/تقرير-التشغيل" element={<OperationReport />} />
               <Route path="/تقرير-المختبر" element={<LabReport />} />
               <Route path="/users" element={<Users />} />
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
-import DashboardFilter from "../features/dashboard/DashboardFilter";
-import DashboardLayout from "../features/dashboard/DashboardLayout";
-import Heading from "../ui/Heading";
-import Row from "../ui/Row";
+// import DashboardFilter from "../features/dashboard/DashboardFilter";
+// import DashboardLayout from "../features/dashboard/DashboardLayout";
+// import Heading from "../ui/Heading";
+// import Row from "../ui/Row";
 
 function Dashboard() {
   return (
